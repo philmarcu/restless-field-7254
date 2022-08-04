8987df60cd988e5c331cce651879d979def5da63
philmarcu <philmark18@gmail.com>
2022-08-04 16:23:20 -0600

    final final refactor to airline model test- adult passengers

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -11,20 +11,24 @@ RSpec.describe Airline, type: :model do
     describe '#adult_passengers' do
       it 'gets all the unique adult passengers for a flight' do
         air_1 = Airline.create!(name: "American")
-        air_2 = Airline.create!(name: "United")
         
         flight_1 = air_1.flights.create!(number: "7990", date: "2/7/2022", departure_city: "Glendale", arrival_city: "Dallas" )
-        flight_2 = air_2.flights.create!(number: "7990", date: "2/7/2022", departure_city: "Glendale", arrival_city: "Dallas" )
+        flight_2 = air_1.flights.create!(number: "7990", date: "2/7/2022", departure_city: "Orlando", arrival_city: "Houston" )
   
   
-        pass_1 = flight_1.passengers.create!(name: "Brennan Lee Mulligan", age: 33)
-        pass_2 = flight_1.passengers.create!(name: "Misty Moore", age: 44)
-        pass_3 = flight_1.passengers.create!(name: "Bill Seacaster", age: 46)
-        pass_4 = flight_1.passengers.create!(name: "Danny Moore", age: 15)
-        pass_5 = flight_2.passengers.create!(name: "Brennan Lee Mulligan", age: 33)
-  
+        pass_1 = Passenger.create!(name: "Brennan Lee Mulligan", age: 33)
+        pass_2 = Passenger.create!(name: "Misty Moore", age: 44)
+        pass_3 = Passenger.create!(name: "Bill Seacaster", age: 46)
+        pass_4 = Passenger.create!(name: "Danny Moore", age: 15)
+
+        fli_pas1 = FlightPassenger.create!(flight_id: flight_1.id, passenger_id: pass_1.id)
+        fli_pas2 = FlightPassenger.create!(flight_id: flight_1.id, passenger_id: pass_2.id)
+        fli_pas3 = FlightPassenger.create!(flight_id: flight_1.id, passenger_id: pass_3.id)
+        fli_pas4 = FlightPassenger.create!(flight_id: flight_1.id, passenger_id: pass_4.id)
+
+        fli_pas2 = FlightPassenger.create!(flight_id: flight_2.id, passenger_id: pass_1.id)
+      
         expect(air_1.adult_passengers).to eq([pass_1, pass_2, pass_3])
-        expect(air_2.adult_passengers).to eq([pass_5])
       end
     end
   end
